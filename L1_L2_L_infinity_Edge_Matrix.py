0c6e37f67c14d92e69dd221a058e09deb147f818
Aravind Raamasamy <62833075+aravindstriker6@users.noreply.github.com>
2022-07-21 01:10:49 -0400

    Update L1_L2_L_infinity_Edge_Matrix.py

--- L1_L2_L_infinity_Edge_Matrix.py
+++ L1_L2_L_infinity_Edge_Matrix.py
@@ -1,96 +1,63 @@
 import numpy as np
 
 
-def same_norm_l2(X_c1_curr, X_c2_curr, epsilon_1, epsilon_2):
-    new = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
+def same_norm_l2(X_c1_curr, X_c2_curr):
+    final_l2_distance = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
     for i in tqdm(range(X_c1_curr.shape[0])):
         for j in range(X_c2_curr.shape[0]):
-            final = np.linalg.norm(X_c1_curr[i] - X_c2_curr[j], ord=2)
-            if final <= epsilon_1 + epsilon_2:
-                new[i][j] = 1
-            else:
-                new[i][j] = 0
-    return new
+            final_l2_distance[i][j] = np.linalg.norm(X_c1_curr[i] - X_c2_curr[j], ord=2)
+    return final_l2_distance
 
 
-def same_norm_l_infinity(X_c1_curr, X_c2_curr, epsilon_1, epsilon_2):
-    new = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
+def same_norm_l_infinity(X_c1_curr, X_c2_curr):
+    final_l_infinity_distance = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
     for i in tqdm(range(X_c1_curr.shape[0])):
         for j in range(X_c2_curr.shape[0]):
-            final = np.linalg.norm(X_c1_curr[i] - X_c2_curr[j], ord=np.inf)
-            if final <= epsilon_1 + epsilon_2:
-                new[i][j] = 1
-            else:
-                new[i][j] = 0
-    return new
+            final_l_infinity_distance[i][j] = np.linalg.norm(X_c1_curr[i] - X_c2_curr[j], ord=np.inf)
+    return final_l_infinity_distance
 
 
-def same_norm_l1(X_c1_curr, X_c2_curr, epsilon_1, epsilon_2):
-    new = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
+def same_norm_l1(X_c1_curr, X_c2_curr):
+    final_l1_distance= np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
     for i in tqdm(range(X_c1_curr.shape[0])):
         for j in range(X_c2_curr.shape[0]):
-            final = np.linalg.norm(X_c1_curr[i] - X_c2_curr[j], ord=1)
-            if final <= epsilon_1 + epsilon_2:
-                new[i][j] = 1
-            else:
-                new[i][j] = 0
-    return new
+            final_l1_distance[i][j] = np.linalg.norm(X_c1_curr[i] - X_c2_curr[j], ord=1)
+    return final_l1_distance
 
-
-def min_l2(x, epsilon):
+def test_l2_infinity(x, epsilon_infinity):
     final_sum = 0
     for i in range(len(x)):
-        if abs(x[i]) >= epsilon:
-            final_sum += (abs(x[i]) - epsilon) ** 2
+        if abs(x[i]) >= epsilon_infinity:
+            final_sum += (abs(x[i]) - epsilon_infinity) ** 2
         else:
             continue
     final_sum = final_sum ** (1 / 2)
     return final_sum
 
-
-def test_l2_infinity(x, epsilon_2, epsilon_infinity):
-    calculate = min_l2(x, epsilon_infinity)
-    if calculate <= epsilon_2:
-        return float(1)
-    else:
-        return float(0)
-
-
-def different_norm_l2_l_infinity(X_c1_curr, X_c2_curr, epsilon_2, epsilon_infinity):
-    new = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
+def different_norm_l2_l_infinity(X_c1_curr, X_c2_curr,epsilon_infinity):
+    distance_l2_l_infinity = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
     for i in (range(X_c1_curr.shape[0])):
         for j in range(X_c2_curr.shape[0]):
             ans = abs(X_c1_curr[i] - X_c2_curr[j])
-            new[i][j] = test_l2_infinity(ans, epsilon_2, epsilon_infinity)
-    return new
+            distance_l2_l_infinity[i][j] = test_l2_infinity(ans,epsilon_infinity)
+    return  distance_l2_l_infinity
 
-
-def min_l1(x, epsilon):
-    final_sum = 0
+def test_l1_infinity(x,epsilon_infinity):
+    final_sum=0
     for i in range(len(x)):
-        if abs(x[i]) >= epsilon:
-            final_sum += (abs(x[i]) - epsilon) ** 2
+        if abs(x[i])>=epsilon_infinity:
+            final_sum+=abs(abs(x[i])-epsilon_infinity)
         else:
             continue
-    final_sum = final_sum ** (1 / 2)
     return final_sum
 
-
-def test_l1_infinity(x, epsilon_2, epsilon_infinity):
-    calculate = min_l1(x, epsilon_infinity)
-    if calculate <= epsilon_2:
-        return float(1)
-    else:
-        return float(0)
-
-
-def different_norm_l1_l_infinity(X_c1_curr, X_c2_curr, epsilon_2, epsilon_infinity):
-    new = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
+def different_norm_l1_l_infinity(X_c1_curr, X_c2_curr,epsilon_infinity):
+    distance_l1_l_infinity= np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
     for i in (range(X_c1_curr.shape[0])):
         for j in range(X_c2_curr.shape[0]):
             ans = abs(X_c1_curr[i] - X_c2_curr[j])
-            new[i][j] = test_l1_infinity(ans, epsilon_2, epsilon_infinity)
-    return new
+            distance_l1_l_infinity[i][j] = test_l1_infinity(ans, epsilon_infinity)
+    return distance_l1_l_infinity
 
 
 def find_z(X):
@@ -134,194 +101,92 @@ def find_epsilon_2(K, ans, lambda_value):
 
 
 # When both the norms are different
-def different_norm_11_l2(X_c1_curr, X_c2_curr, epsilon_1, epsilon2):
-    new = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
+def different_norm_11_l2(X_c1_curr, X_c2_curr, epsilon_1):
+    distance_l1_l2 = np.zeros([X_c1_curr.shape[0], X_c2_curr.shape[0]])
     for i in (range(X_c1_curr.shape[0])):
         for j in range(X_c2_curr.shape[0]):
             ans = abs(X_c1_curr[i] - X_c2_curr[j])
             X, K, lambda_value = test_l1_l2(ans, epsilon_1)
-            min_epsilon_2 = find_epsilon_2(K, X, lambda_value)
-            if epsilon2 >= min_epsilon_2:
-                new[i][j] = 1
-            else:
-                new[i][j] = 0
-
-    return new
+            distance_l1_l2[i][j]= find_epsilon_2(K, X, lambda_value)
+    return distance_l1_l2 
 
 
 def edge_matrix_calculator(epsilon_user, X_c1_curr, X_c2_curr, storage_same_metrics, storage_different_metrics):
     total_edge_matrix = np.empty([len(epsilon_user), len(epsilon_user)], dtype=float)
     edge_matrixes = {}
     total_edges = 0
-    for i in range(tqdm(len(epsilon_user))):
-        for j in range(tqdm(len(epsilon_user))):
+    for i in range((len(epsilon_user))):
+        for j in range((len(epsilon_user))):
             if epsilon_user[i][0] == epsilon_user[j][0]:
-                if epsilon_user[i][0] == 'l1' and epsilon_user[j][0] == 'l1':
-                    key = [epsilon_user[i][0], epsilon_user[i][1], epsilon_user[j][1]]
-                    key.sort()
-                    key = tuple(key)
+                    epsilon_1=epsilon_user[i][1]
+                    epsilon_2=epsilon_user[j][1]
+                    key = epsilon_user[i][0]
                     if key in storage_same_metrics:
-                        new = storage_same_metrics[key]
-                        edge_matrixes[i, j] = new
-                        total_edge_matrix[i][j] = new.sum()
-                        total_edges += new.sum()
+                        distance_matrix= storage_same_metrics[key]
                     else:
-                        new = same_norm_l1(X_c1_curr, X_c2_curr, epsilon_user[i][1], epsilon_user[j][1])
-                        new = new.astype(float)
-                        edge_matrixes[i, j] = new
-                        total_edge_matrix[i][j] = new.sum()
-                        total_edges += new.sum()
-                        key = [epsilon_user[i][0], epsilon_user[i][1], epsilon_user[j][1]]
-                        key.sort()
-                        key = tuple(key)
-                        storage_same_metrics[key] = new
-                elif epsilon_user[i][0] == 'l2' and epsilon_user[j][0] == 'l2':
-                    key = [epsilon_user[i][0], epsilon_user[i][1], epsilon_user[j][1]]
-                    key.sort()
-                    key = tuple(key)
-                    if key in storage_same_metrics:
-                        new = storage_same_metrics[key]
-                        edge_matrixes[i, j] = new
-                        total_edge_matrix[i][j] = new.sum()
-                        total_edges += new.sum()
-                    else:
-                       new = same_norm_l2(X_c1_curr, X_c2_curr, epsilon_user[i][1], epsilon_user[j][1])
-                       new = new.astype(float)
-                       edge_matrixes[i, j] = new
-                       total_edge_matrix[i][j] = new.sum()
-                       total_edges += new.sum()
-                       key = [epsilon_user[i][0], epsilon_user[i][1], epsilon_user[j][1]]
-                       key.sort()
-                       key = tuple(key)
-                    storage_same_metrics[key] = new
-                else:
-                    key = [epsilon_user[i][0], epsilon_user[i][1], epsilon_user[j][1]]
-                    key.sort()
-                    key = tuple(key)
-                    if key in storage_same_metrics:
-                      new = storage_same_metrics[key]
-                      edge_matrixes[i, j] = new
-                      total_edge_matrix[i][j] = new.sum()
-                      total_edges += new.sum()
-                    else:
-                         new = same_norm_l_infinity(X_c1_curr, X_c2_curr, epsilon_user[i][1], epsilon_user[j][1])
-                         new = new.astype(float)
-                         edge_matrixes[i, j] = new
-                         total_edge_matrix[i][j] = new.sum()
-                         total_edges += new.sum()
-                         key = [epsilon_user[i][0], epsilon_user[i][1], epsilon_user[j][1]]
-                         key.sort()
-                         key = tuple(key)
-                         storage_same_metrics[key] = new
-
-            elif epsilon_user[i][0] == 'l1' and epsilon_user[j][0] == 'l2':
-                 epsilon_1 = epsilon_user[i][1]
-                 epsilon_2 = epsilon_user[j][1]
-                 key = (('l1', epsilon_1), ('l2', epsilon_2))
-                 if key in storage_different_metrics:
-                    new = storage_different_metrics[(('l1', epsilon_1), ('l2', epsilon_2))]
-                    edge_matrixes[i, j] = new
-                    total_edge_matrix[i][j] = new.sum()
-                    total_edges += new.sum()
+                        if epsilon_user[i][0]=='l1':
+                         distance_matrix = same_norm_l1(X_c1_curr, X_c2_curr)
+                        elif epsilon_user[i][0]=='l2':
+                         distance_matrix = same_norm_l2(X_c1_curr, X_c2_curr)
+                        else:
+                          distance_matrix = same_norm_l_infinity(X_c1_curr, X_c2_curr) 
+                        storage_same_metrics[epsilon_user[i][0]] = distance_matrix
+                    new=distance_matrix<=epsilon_1+epsilon_2
+                        
+            elif (epsilon_user[i][0] == 'l1' and epsilon_user[j][0] == 'l2') or (epsilon_user[i][0] == 'l2' and epsilon_user[j][0] == 'l1') :
+                 if epsilon_user[i][0] == 'l1' and epsilon_user[j][0] == 'l2':
+                   epsilon_1 = epsilon_user[i][1]
+                   epsilon_2 = epsilon_user[j][1]
                  else:
-                   new = different_norm_11_l2(X_c1_curr, X_c2_curr, epsilon_1, epsilon_2)
-                   new = new.astype(float)
-                   edge_matrixes[i, j] = new
-                   total_edge_matrix[i][j] = new.sum()
-                   total_edges += new.sum()
-                   key = (('l1', epsilon_1), ('l2', epsilon_2))
-                   storage_different_metrics[key] = new
-            elif epsilon_user[i][0] == 'l2' and epsilon_user[j][0] == 'l1':
-
-                  epsilon_2 = epsilon_user[i][1]
-                  epsilon_1 = epsilon_user[j][1]
-                  key = (('l1', epsilon_1), ('l2', epsilon_2))
-                  if key in storage_different_metrics:
-                      new = storage_different_metrics[(('l1', epsilon_1), ('l2', epsilon_2))]
-                      edge_matrixes[i, j] = new
-                      total_edge_matrix[i][j] = new.sum()
-                      total_edges += new.sum()
-                  else:
-                       new = different_norm_11_l2(X_c1_curr, X_c2_curr, epsilon_1, epsilon_2)
-                       new = new.astype(float)
-                       edge_matrixes[i, j] = new
-                       total_edge_matrix[i][j] = new.sum()
-                       total_edges += new.sum()
-                       key = (('l1', epsilon_1), ('l2', epsilon_2))
-                       storage_different_metrics[key] = new
-
-            elif epsilon_user[i][0] == 'l1' and epsilon_user[j][0] == 'l_infinity':
-
-                  epsilon_1 = epsilon_user[i][1]
-                  epsilon_infinity = epsilon_user[j][1]
-                  key = (('l1', epsilon_1), ('l_infinity', epsilon_infinity))
-                  if key in storage_different_metrics:
-                       new = storage_different_metrics[key]
-                       edge_matrixes[i, j] = new
-                       total_edge_matrix[i][j] = new.sum()
-                       total_edges += new.sum()
-                  else:
-                   new = different_norm_l1_l_infinity(X_c1_curr, X_c2_curr, epsilon_1, epsilon_infinity)
-                   new = new.astype(float)
-                   edge_matrixes[i, j] = new
-                   total_edge_matrix[i][j] = new.sum()
-                   total_edges += new.sum()
-                   key = (('l1', epsilon_1), ('l_infinity', epsilon_infinity))
-                   storage_different_metrics[key] = new
-            elif epsilon_user[i][0] == 'l_infinity' and epsilon_user[j][0] == 'l1':
-
-                epsilon_infinity = epsilon_user[i][1]
-                epsilon_1 = epsilon_user[j][1]
-                key = (('l1', epsilon_1), ('l_infinity', epsilon_infinity))
-                if key in storage_different_metrics:
-                      new = storage_different_metrics[key]
-                      edge_matrixes[i, j] = new
-                      total_edge_matrix[i][j] = new.sum()
-                      total_edges += new.sum()
-                else:
-
-                    new = different_norm_l1_l_infinity(X_c1_curr, X_c2_curr, epsilon_1, epsilon_infinity)
-                    new = new.astype(float)
-                    edge_matrixes[i, j] = new
-                    total_edge_matrix[i][j] = new.sum()
-                    total_edges += new.sum()
-                    key = (('l1', epsilon_1), ('l_infinity', epsilon_infinity))
-                    storage_different_metrics[key] = new
-            elif epsilon_user[i][0] == 'l2' and epsilon_user[j][0] == 'l_infinity':
-
-                epsilon_2 = epsilon_user[i][1]
-                epsilon_infinity = epsilon_user[j][1]
-                key = (('l2', epsilon_2), ('l_infinity', epsilon_infinity))
-                if key in storage_different_metrics:
-                  new = storage_different_metrics[key]
-                  edge_matrixes[i, j] = new
-                  total_edge_matrix[i][j] = new.sum()
-                  total_edges += new.sum()
-                else:
-                  new = different_norm_l2_l_infinity(X_c1_curr, X_c2_curr, epsilon_2, epsilon_infinity)
-                  new = new.astype(float)
-                  edge_matrixes[i, j] = new
-                  total_edge_matrix[i][j] = new.sum()
-                  total_edges += new.sum()
-                  key = (('l2', epsilon_2), ('l_infinity', epsilon_infinity))
-                  storage_different_metrics[key] = new
+                   epsilon_1 = epsilon_user[j][1]
+                   epsilon_2 = epsilon_user[i][1]
+              
+                 key = ('l1','l2',epsilon_1)
+                 if key in storage_different_metrics:
+                    distance_matrix= storage_different_metrics[key]
+                 else:
+                   distance_matrix= different_norm_11_l2(X_c1_curr, X_c2_curr, epsilon_1)
+                   key = ('l1','l2',epsilon_1)
+                   storage_different_metrics[key] = distance_matrix
+                 new=distance_matrix<=epsilon_2
+                    
+            elif (epsilon_user[i][0] == 'l1' and epsilon_user[j][0] == 'l_infinity') or (epsilon_user[i][0] == 'l_infinity' and epsilon_user[j][0] == 'l1') :
+                 if epsilon_user[i][0] == 'l1' and epsilon_user[j][0] == 'l_infinity':
+                   epsilon_1 = epsilon_user[i][1]
+                   epsilon_infinity = epsilon_user[j][1]
+                 else:
+                   epsilon_1 = epsilon_user[j][1]
+                   epsilon_infinity = epsilon_user[i][1]
+              
+                 key = ('l1','l_infinity',epsilon_infinity)
+                 if key in storage_different_metrics:
+                    distance_matrix= storage_different_metrics[key]
+                 else:
+                   distance_matrix= different_norm_l1_l_infinity(X_c1_curr, X_c2_curr,epsilon_infinity)
+                   key =('l1','l_infinity',epsilon_infinity)
+                   storage_different_metrics[key] = distance_matrix
+                 new=distance_matrix<=epsilon_1
             else:
-                epsilon_infinity = epsilon_user[i][1]
-                epsilon_2 = epsilon_user[j][1]
-                key = (('l2', epsilon_2), ('l_infinity', epsilon_infinity))
-                if key in storage_different_metrics:
-                      new = storage_different_metrics[key]
-                      edge_matrixes[i, j] = new
-                      total_edge_matrix[i][j] = new.sum()
-                      total_edges += new.sum()
-                else:
-                    new = different_norm_l2_l_infinity(X_c1_curr, X_c2_curr, epsilon_2, epsilon_infinity)
-                    new = new.astype(float)
-                    edge_matrixes[i, j] = new
-                    total_edge_matrix[i][j] = new.sum()
-                    total_edges += new.sum()
-                    key = (('l2', epsilon_2), ('l_infinity', epsilon_infinity))
-                    storage_different_metrics[key] = new
+                 if epsilon_user[i][0] == 'l2' and epsilon_user[j][0] == 'l_infinity':
+                   epsilon_2 = epsilon_user[i][1]
+                   epsilon_infinity = epsilon_user[j][1]
+                 else:
+                   epsilon_2 = epsilon_user[j][1]
+                   epsilon_infinity = epsilon_user[i][1]
+              
+                 key = ('l2','l_infinity',epsilon_infinity)
+                 if key in storage_different_metrics:
+                    distance_matrix= storage_different_metrics[key]
+                 else:
+                   distance_matrix= different_norm_l1_l_infinity(X_c1_curr, X_c2_curr,epsilon_infinity)
+                   key =('l1','l_infinity',epsilon_infinity)
+                   storage_different_metrics[key] = distance_matrix
+                 new=distance_matrix<=epsilon_2
+            new = new.astype(float)
+            edge_matrixes[i, j] = new
+            total_edge_matrix[i][j] = new.sum()
+            total_edges += new.sum()
+
     stacked = []
     for i in range(len(epsilon_user)):
       h_stack = []
